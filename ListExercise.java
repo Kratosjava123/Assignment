d2d58be1d867d19bee5d509f897174cb760090e6
Nikhil <93237125+Kratosjava123@users.noreply.github.com>
2021-12-05 23:45:07 +0530

    Add files via upload

---
+++ ListExercise.java
@@ -0,0 +1,24 @@
+
+import java.util.ArrayList;
+import java.util.List;
+    public class ListExercise  {
+        public static void main(String[] args){
+
+            List list = new arrayslist<String>(InitialCapacity:10);
+
+            list.add("monday");
+            list.add("tuesday");
+            list.add("wednesday");
+            list.add("thursday");
+            list.add("friday");
+            list.add("saturday");
+            list.add("sunday");
+            System.out.println(list);
+
+            int index;
+            System.out.println(list.remove(index;:5));
+
+            System.out.println(list.get(6));
+        }
+    }
+
